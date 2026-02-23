dd8bee9c0c777fce7c681aa41cafc2d4ccd4b62e
jeremytey <jeremy8598@gmail.com>
2026-02-23 14:35:37 +0800

    testing

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 const {Client} = require('pg');
-const SQL = require('schema.sql');
-
-
 
 async function main() {
     const client = new Client({
         connectionString: process.env.DATABASE_URL
     });
     await client.connect();
-    await client.query(SQL);
+    await client.query(`
+        INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *;
+    `, ['Basketball', 'All basketball related items']);
+    await client.query(`
+        INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *;
+    `, ['Soccer', 'All soccer related items']);
+    await client.query(`
+        INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *;
+    `, ['Tennis', 'All tennis related items']);
     await client.end();
 }
 
